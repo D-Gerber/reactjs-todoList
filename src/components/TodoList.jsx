1425b220d2f2241a29f1ef51ce94c234daa1b757
Daniel <drgerber69@gmail.com>
2024-06-19 17:03:12 +0200

    Update TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 
 export default function TodoList() {
-   let todos = [
-    'Go to the gym',
-    'Eat more fruits and vegetables',
-    'Pick up kids from school'
-   ]
-    
-   return (
+    let todos = [
+        'Go to the gym',
+        'Eat more fruits and vegetables',
+        'Pick up kids from school'
+    ]
+
+    return (
         <ul className="main">
             {todos.map((todo, todoIndex) => {
                 return (
-                    <li className='todoItem' key={todoIndex}>{todo}</li>
+                    <li className='todoItem' key={todoIndex}>
+                        {todo}
+                        <i class="fa fa-pencil-square-o" aria-hidden="true"></i>
+                    </li>
                 )
             })}
         </ul>
