e87f20e914972a87fa939210048d1ce2514e9f95
Daniel <drgerber69@gmail.com>
2024-06-19 17:13:56 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import TodoInput from "./components/TodoInput";
 import TodoList from "./components/TodoList";
 
 function App() {
+  let todos = [
+    'Go to the gym',
+    'Eat more fruits and vegetables',
+    'Pick up kids from school'
+]
+
   return (
     <>
       <TodoInput />
