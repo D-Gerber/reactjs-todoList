75f7ba22868d226115420fac6b90aa9b1b6ce6ee
Daniel <drgerber69@gmail.com>
2024-06-19 17:24:11 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
-import TodoInput from "./components/TodoInput";
-import TodoList from "./components/TodoList";
+import { useState } from "react"
+import TodoInput from "./components/TodoInput"
+import TodoList from "./components/TodoList"
 
 function App() {
-  let todos = [
+  const [todos, setTodos] = useState([
     'Get up early and study',
     'Take 15 min breaks in between',
     'Get snacks and continue studying'
-  ]
+  ])
 
   return (
     <>
