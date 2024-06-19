e238810772429584d0683a81596d1238a20bdf9e
Daniel <drgerber69@gmail.com>
2024-06-19 17:55:56 +0200

    Update TodoList.jsx

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -8,7 +8,8 @@ export default function TodoList(props) {
         <ul className="main">
             {todos.map((todo, todoIndex) => {
                 return (
-                    <TodoCard {...props} key={todoIndex}>
+                    <TodoCard {...props} key={todoIndex}
+                        index={todoIndex}>
                         <p>{todo}</p>
                     </TodoCard>
                 )
