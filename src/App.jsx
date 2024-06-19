8e6e248536c976579ab535f061e0df1621d212fc
Daniel <drgerber69@gmail.com>
2024-06-19 15:25:26 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import TodoInput from "./components/TodoInput"
-import TodoList from "./components/TodoList"
+import TodoInput from "./components/TodoInput";
+import TodoList from "./components/TodoList";
 
 function App() {
-
   return (
     <main>
       <TodoInput />
       <TodoList />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
