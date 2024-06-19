5cdb8777d8225622156217675d942666088efa96
Daniel <drgerber69@gmail.com>
2024-06-19 17:17:49 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import TodoList from "./components/TodoList";
 
 function App() {
   let todos = [
-    'Go to the gym',
-    'Eat more fruits and vegetables',
-    'Pick up kids from school'
+    'Get up early and study',
+    'Take 15 min breaks in between',
+    'Get snacks and continue studying'
   ]
 
   return (
